7eb7c4ce50ca79752800cb090d2b5424ed0382d6
izumi <yugoizumi@outlook.com>
2025-03-27 20:20:53 +0900

    ジオメトリ形状の追加

--- core/include/Subsystem/Render/Shape/PrimitiveGeometryShape.h
+++ core/include/Subsystem/Render/Shape/PrimitiveGeometryShape.h
@@ -70,6 +70,113 @@ namespace MyosotisFW::System::Render::Shape
 
 		return mesh;
 	}
+
+	inline Mesh createPlane(float size = 1.0f, glm::vec4 color = { 1.0f,1.0f,1.0f,1.0f }, glm::vec3 center = { 0.0f,0.0f,0.0f })
+	{
+		float halfSize = size * 0.5f;
+		Mesh mesh = {};
+		// 頂点データ (x, y, z, w, r, g, b, a, nx, ny, nz)
+		mesh.vertex = {
+			// 前面 (0, 0, -1)
+			-halfSize + center.x, -halfSize + center.y, 0.0f + center.z,  1.0,  0.0,  0.0, -1.0, color.r, color.g, color.b, color.a,	// 0
+			 halfSize + center.x, -halfSize + center.y, 0.0f + center.z,  1.0,  0.0,  0.0, -1.0, color.r, color.g, color.b, color.a,	// 1
+			 halfSize + center.x,  halfSize + center.y, 0.0f + center.z,  1.0,  0.0,  0.0, -1.0, color.r, color.g, color.b, color.a,	// 2
+			-halfSize + center.x,  halfSize + center.y, 0.0f + center.z,  1.0,  0.0,  0.0, -1.0, color.r, color.g, color.b, color.a,	// 3
+		};
+		mesh.index = {
+			0,	2,	1,	0,	3,	2,	// 前面
+		};
+		return mesh;
+	}
+
+	inline Mesh createCircle(float size = 1.0f, glm::vec4 color = { 1.0f,1.0f,1.0f,1.0f }, uint32_t side = 12, glm::vec3 center = { 0.0f,0.0f,0.0f })
+	{
+		float radius = size * 0.5f;
+		Mesh mesh = {};
+
+		ASSERT(side > 2, "circle's side must be more than 2");
+
+		// 頂点データ (x, y, z, w, r, g, b, a, nx, ny, nz)
+		float a = glm::two_pi<float>() / static_cast<float>(side);
+
+		mesh.vertex.insert(mesh.vertex.end(), {
+			center.x, center.y, center.z, 1.0f,
+			0.0f, 0.0f, -1.0f,
+			0.0f, 0.0f,
+			color.r, color.g, color.b, color.a });
+		mesh.vertex.insert(mesh.vertex.end(), {
+			center.x + radius * sinf(a), center.y + radius * cosf(a), center.z, 1.0f,
+			0.0f, 0.0f, -1.0f,
+			0.0f, 0.0f,
+			color.r, color.g, color.b, color.a });
+
+		for (uint32_t i = 1; i <= side; i++)
+		{
+			uint32_t idx = i + 1;
+			idx = idx > side ? idx - side : idx;
+			mesh.vertex.insert(mesh.vertex.end(), {
+				center.x + radius * sinf(a * static_cast<float>(idx)), center.y + radius * cosf(a * static_cast<float>(idx)), center.z, 1.0f,
+				0.0f, 0.0f, -1.0f,
+				0.0f, 0.0f,
+				color.r, color.g, color.b, color.a });
+			mesh.index.insert(mesh.index.end(), { 0, i, idx });
+		}
+		return mesh;
+	}
+
+	inline Mesh createSphere(float size = 1.0f, glm::vec4 color = { 1.0f,1.0f,1.0f,1.0f }, uint32_t side = 12, glm::vec3 center = { 0.0f,0.0f,0.0f })
+	{
+		float radius = size * 0.5f;
+		Mesh mesh = {};
+		ASSERT(side > 3, "sphere's side must be more than 3");
+
+		// 頂点データ (x, y, z, w, r, g, b, a, nx, ny, nz)
+		for (uint32_t i = 1; i <= side; i++)
+		{
+			float phi = glm::pi<float>() * static_cast<float>(i) / static_cast<float>(side);
+
+			for (uint32_t j = 0; j <= side; j++)
+			{
+				float therte = glm::two_pi<float>() * static_cast<float>(j) / static_cast<float>(side);
+				glm::vec3 position = {
+					center.x + radius * sinf(phi) * cosf(therte),
+					center.y + radius * cosf(phi),
+					center.z + radius * sinf(phi) * sinf(therte)
+				};
+				glm::vec3 normal = glm::normalize(position - center);
+				glm::vec2 uv = { static_cast<float>(j) / static_cast<float>(side), static_cast<float>(i) / static_cast<float>(side) };
+
+				mesh.vertex.insert(mesh.vertex.end(), {
+					position.x, position.y, position.z, 1.0f,
+					normal.x, normal.y, normal.z,
+					uv.x, uv.y,
+					color.r, color.g, color.b, color.a });
+			}
+
+			uint32_t idx = i + 1;
+			idx = idx > side ? idx - side : idx;
+			mesh.vertex.insert(mesh.vertex.end(), {
+				center.x + radius * sinf(b * static_cast<float>(i)) * sinf(a * static_cast<float>(idx)),
+				center.y + radius * sinf(b * static_cast<float>(i)) * cosf(a * static_cast<float>(idx)),
+				center.z + radius * cosf(b * static_cast<float>(i)),
+				1.0f,
+				0.0f, 0.0f, -1.0f,
+				0.0f, 0.0f,
+				color.r, color.g, color.b, color.a });
+			mesh.index.insert(mesh.index.end(), { 0, i, idx });
+		}
+
+		for (uint32_t i = 1; i <= side; i++)
+		{
+			for (uint32_t j = 0; j <= side; j++)
+			{
+				uint32_t idx = i * (side + 1) + j;
+				mesh.index.insert(mesh.index.end(), { idx + 1, idx + side + 1, idx });
+				mesh.index.insert(mesh.index.end(), { idx + 1, idx + side + 2, idx + side + 1 });
+			}
+		}
+		return mesh;
+	}
 }
 
 
