d24bbb997f5ab7a1f5cb04c4e1a6dbf221ebf6b5
kong9812 <choiyaukong19981229@gmail.com>
2025-03-30 15:28:55 +0900

    fix typo

--- core/include/Subsystem/Render/StaticMesh/PrimitiveGeometry.h
+++ core/include/Subsystem/Render/StaticMesh/PrimitiveGeometry.h
@@ -13,13 +13,13 @@ namespace MyosotisFW::System::Render
 
 		virtual const ObjectType GetObjectType() const override { return ObjectType::PrimitiveGeometryMesh; }
 
-		void PrepareForRender(const RenderDevice_ptr& device, const RenderResources_ptr& resources)override;
+		void PrepareForRender(const RenderDevice_ptr& device, const RenderResources_ptr& resources) override;
 		void Update(const UpdateData& updateData, const Camera::CameraBase_ptr& camera) override;
 		void BindCommandBuffer(const VkCommandBuffer& commandBuffer, const RenderPipelineType& pipelineType) override;
 
 		glm::vec4 GetCullerData() override;
-		virtual rapidjson::Value Serialize(rapidjson::Document::AllocatorType& allocator) const { return __super::Serialize(allocator); }
-		virtual void Deserialize(const rapidjson::Value& doc, std::function<void(ObjectType, const rapidjson::Value&)> createObject) { __super::Deserialize(doc, createObject); }
+		virtual rapidjson::Value Serialize(rapidjson::Document::AllocatorType& allocator) const override { return __super::Serialize(allocator); }
+		virtual void Deserialize(const rapidjson::Value& doc, const std::function<void(ObjectType, const rapidjson::Value&)>& createObject) override { __super::Deserialize(doc, createObject); }
 	private:
 		void loadAssets() override;
 		void prepareShaderStorageBuffers() override {};
